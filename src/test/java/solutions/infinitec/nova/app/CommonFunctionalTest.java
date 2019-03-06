9cbeeb69d2c781211ee4a29ccfc75e569dcd14ac
tugcekonuklar <tugce.konuklar@infinitec.solutions>
2019-03-06 13:27:50 +0100

    delete execute event method

--- src/test/java/solutions/infinitec/nova/app/CommonFunctionalTest.java
+++ src/test/java/solutions/infinitec/nova/app/CommonFunctionalTest.java
@@ -3,14 +3,11 @@ package solutions.infinitec.nova.app;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -29,17 +26,9 @@ public abstract class CommonFunctionalTest {
   @LocalServerPort
   private int serverPort;
 
-  @Autowired
-  private ApplicationEventPublisher eventPublisher;
-
   @BeforeAll
   public static void before() {
     RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-  }
-
-  @AfterEach
-  public void afterEach() {
-    eventPublisher.publishEvent(new TestMethodExecutedEvent());
   }
 
   public RequestSpecification requestSpecification() {
--- src/test/java/solutions/infinitec/nova/app/TestMethodExecutedEvent.java
+++
@@ -1,7 +0,0 @@
-package solutions.infinitec.nova.app;
-
-/**
- * Event published after every test-method execution.
- */
-public class TestMethodExecutedEvent {
-}
